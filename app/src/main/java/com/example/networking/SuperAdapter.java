2d5cd748a262032300638ebd166692470770970d
Hampus Wetterlind <b21hamwe@student.his.se>
2022-05-16 15:41:17 +0200

    Created functions in the adapter file

--- app/src/main/java/com/example/networking/SuperAdapter.java
+++ app/src/main/java/com/example/networking/SuperAdapter.java
@@ -1,4 +1,30 @@
 package com.example.networking;
 
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SuperAdapter {
+
+    private List<Mountain> mountains = new ArrayList();
+
+    public MountainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+
+    };
+
+    public void onBindViewHolder(@NonNull MountainViewHolder holder, int position){
+
+    };
+
+
+    public int getItemCount(){
+      return mountains.size();
+    };
+
+    public void setMountains (List<Mountain>mountains){
+        this.mountains = mountains;
+    };
 }
