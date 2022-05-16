7445ee7b89bec2e108c55acfb36e65910240e554
Hampus Wetterlind <b21hamwe@student.his.se>
2022-05-16 15:26:00 +0200

    Created Local member Variables

---
+++ app/src/main/java/com/example/networking/ViewHolder.java
@@ -0,0 +1,19 @@
+package com.example.networking;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView height;
+    private TextView name;
+    private TextView Location;
+
+
+    public MountainViewHolder(@NonNull View itemView){
+        height =
+    }
+}
